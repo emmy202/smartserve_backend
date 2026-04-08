a991a61884a26b09a0da441743c0487879819f75
Emmanue Hitimana <emmanuel.hitimana@aos.rw>
2026-04-08 15:58:12 +0200

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const origins = process.env.CORS_ORIGINS?.split(',') || ['http://localhost:5173', 'http://localhost:3001'];
+  const corsOrigins = process.env.CORS_ORIGINS === '*' 
+    ? true 
+    : process.env.CORS_ORIGINS?.split(',').map(o => o.trim()) || ['http://localhost:5173', 'http://localhost:3001'];
+
   app.enableCors({
-    origin: origins,
+    origin: corsOrigins,
     credentials: true,
   });
   app.setGlobalPrefix('api');
